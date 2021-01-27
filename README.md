bcc32f9d13ad730595744352abab9e69ecfe412e
Kyle Moore <github@kylemoore.com>
2021-01-27 07:51:36 -0800

    Add example project and update README
     - Fortunately the example works!

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gradle-publication-configuration
 Demonstrates challenges related to creating and configuring a publication in separate steps
+
+## Steps to reproduce
+1. `./gradlew :lib:generateDescriptorFileForFooPublication`
+2. `less lib/build/publications/foo/ivy.xml`
+
+This example successfully creates a publication (from the built-in `java` component) in a binary plugin,
+then configures the publication in a build script.  A custom artifact and configuration are added to the ivy descriptor.
---
+++ buildSrc/src/main/java/com/kylemoore/foo/CreatesFooPublicationPlugin.java
@@ -0,0 +1,24 @@
+package com.kylemoore.foo;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.publish.PublishingExtension;
+import org.gradle.api.publish.ivy.IvyPublication;
+import org.gradle.api.publish.ivy.plugins.IvyPublishPlugin;
+import org.gradle.api.publish.tasks.GenerateModuleMetadata;
+
+public class CreatesFooPublicationPlugin implements Plugin<Project> {
+
+  @Override
+  public void apply(Project project) {
+    project.getPlugins().apply(JavaPlugin.class);
+    project.getPlugins().apply(IvyPublishPlugin.class);
+    PublishingExtension publishing = project.getExtensions().getByType(PublishingExtension.class);
+    publishing.getPublications().create("foo", IvyPublication.class, p -> {
+      p.from(project.getComponents().getByName("java"));
+    });
+    project.getTasks().withType(GenerateModuleMetadata.class).configureEach(t -> t.setEnabled(false));
+  }
+
+}
