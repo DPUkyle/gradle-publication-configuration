c31a50dc3312aa99903ab8b8a82be0e4f3a961f2
Kyle Moore <github@kylemoore.com>
2021-01-29 11:43:57 -0800

    Add working example, using Project#afterEvaluate to lazily configure publications
     - See https://github.com/gradle/gradle/issues/12026 for a similar issue

--- buildSrc/src/main/java/com/kylemoore/foo/CreatesFooPublicationPlugin.java
+++ buildSrc/src/main/java/com/kylemoore/foo/CreatesFooPublicationPlugin.java
@@ -1,12 +1,16 @@
 package com.kylemoore.foo;
 
+import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.plugins.JavaBasePlugin;
 import org.gradle.api.plugins.JavaPlugin;
+import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.publish.PublishingExtension;
 import org.gradle.api.publish.ivy.IvyPublication;
 import org.gradle.api.publish.ivy.plugins.IvyPublishPlugin;
 import org.gradle.api.publish.tasks.GenerateModuleMetadata;
+import org.gradle.api.tasks.bundling.Jar;
 
 public class CreatesFooPublicationPlugin implements Plugin<Project> {
 
@@ -21,11 +25,41 @@ public class CreatesFooPublicationPlugin implements Plugin<Project> {
     project.getTasks().withType(GenerateModuleMetadata.class).configureEach(t -> t.setEnabled(false));
 
     // now, add a custom configuration to the publication
-    //configureFooPublication(fooPublication);
+    configureFooPublication(project, fooPublication);
+
+    // create sources jar
+    Jar sourcesJar = project.getTasks().create("sourcesJar", Jar.class);
+    sourcesJar.dependsOn(project.getTasks().getByName(JavaPlugin.CLASSES_TASK_NAME));
+    sourcesJar.setGroup(JavaBasePlugin.DOCUMENTATION_GROUP);
+    sourcesJar.setDescription("Generates a jar file containing the sources for the main source code.");
+    sourcesJar.getArchiveClassifier().set("sources");
+
+    sourcesJar.from(project.getConvention().getPlugin(JavaPluginConvention.class).getSourceSets().getByName("main").getAllSource());
   }
 
-  private void configureFooPublication(IvyPublication fooPublication) {
-    fooPublication.configurations(configurations -> configurations.create("docs"));
+  /**
+   * {@link Project#afterEvaluate(Action)} is necessary due to limitations in the IvyPublication implementation
+   * related to the Software Model.
+   *
+   * Premature configuration of the publication will cause the Software Component to be evaluated
+   * too soon, producing a nearly empty descriptor.
+   *
+   * @see <a href="https://github.com/gradle/gradle/issues/12026">gradle/gradle#12026</a>
+   * @param project the project containing the publication
+   * @param fooPublication the publication to configure, after the project is evaluated
+   */
+  private void configureFooPublication(Project project, IvyPublication fooPublication) {
+    project.afterEvaluate( p -> {
+      fooPublication.configurations(configurations -> configurations.create("docs", docs -> {
+        docs.extend("javadoc");
+        docs.extend("sources");
+      }));
+
+      fooPublication.configurations(configurations -> configurations.create("sources"));
+      fooPublication.artifact(project.getTasks().withType(Jar.class).getByName("sourcesJar"), sources -> {
+        sources.setConf("sources");
+      });
+    });
   }
 
 }
